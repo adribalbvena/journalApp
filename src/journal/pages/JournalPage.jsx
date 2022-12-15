a93b0782f51791a7ea3a59b9ffc9750019701f08
Adriana Balbuena <adribalbuena@hotmail.com>
2022-12-15 18:02:22 +0100

    JournalLayout and JournalPage setted

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,10 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-       <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+       <Typography>Cillum tempor amet incididunt dolore et culpa veniam laborum irure voluptate fugiat sint amet. Esse in tempor officia adipisicing voluptate amet anim ullamco Lorem ad ad sint incididunt. Anim do ipsum adipisicing enim ut enim magna officia incididunt ullamco. Dolor irure nisi in qui voluptate laborum sint ullamco non dolor ad. Officia adipisicing duis consectetur ut fugiat nulla irure fugiat magna.</Typography>
+    </JournalLayout>
   )
 }
