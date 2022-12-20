aec6676b6962b45368c412af16271b4e0b9c069f
Adriana Balbuena <adribalbuena@hotmail.com>
2022-12-20 12:39:59 +0100

    google signin config w redux finished

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -12,10 +12,22 @@ export const authSlice = createSlice({
 
     },
     reducers: {
-        login: ( state, action ) => {
+        login: ( state, { payload } ) => {
+            state.status= 'authenticated', // 'checking' , 'authenticated'
+            state.uid= payload.uid;
+            state.email= payload.email;
+            state.displayName= payload.displayName;
+            state.photoURL= payload.payload;
+            state.errorMessage= null;
 
         },
-        logout: ( state, payload ) => {
+        logout: ( state, { payload } ) => {
+            state.status= 'not-authenticated', // 'checking' , 'authenticated'
+            state.uid= null;
+            state.email= null;
+            state.displayName= null;
+            state.photoURL= null;
+            state.errorMessage= payload.errorMessage;
 
         },
         checkingCredentials: (state) => {
