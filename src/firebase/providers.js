c55588ddfa48468db7f453ef17611e49d633a4ed
Adriana Balbuena <adribalbuena@hotmail.com>
2022-12-20 19:57:24 +0100

    register and authentication added

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, updateProfile } from "firebase/auth";
 import { FirebaseAuth } from "./config";
 
 const googleProvider = new GoogleAuthProvider();
@@ -29,5 +29,27 @@ export const signInWithGoogle = async() => {
             errorMessage
         }
 
+    }
+}
+
+export const registerUserWithEmailPassword = async({ email, password, displayName }) => {
+    try {
+
+        const resp = await createUserWithEmailAndPassword( FirebaseAuth, email, password );
+        const { uid, photoURL } = resp.user;
+
+        //update displayName in firebase
+        await updateProfile( FirebaseAuth.currentUser, { displayName });
+
+        return {
+            ok: true,
+            uid, photoURL, email, displayName
+        }
+
+    } catch (error) {
+        return {
+            ok: false,
+            errorMessage: error.message
+        }
     }
 }
