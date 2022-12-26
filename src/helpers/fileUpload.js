b04b3bf99ffc5497fbb26a962e62f7ec2454e104
Adriana Balbuena <adribalbuena@hotmail.com>
2022-12-26 16:10:47 +0100

    uploading files to cloudinary

---
+++ src/helpers/fileUpload.js
@@ -0,0 +1,27 @@
+export const fileUpload = async( file ) => {
+    if ( !file ) throw new Error('There is no image to upload');
+
+    const cloudUrl = 'https://api.cloudinary.com/v1_1/reactapps/upload';
+
+    const formData = new FormData();
+    formData.append('upload_preset','journal');
+    formData.append('file', file );
+
+    try {
+ 
+        const resp = await fetch( cloudUrl, {
+            method: 'POST',
+            body: formData
+        })
+
+        if ( !resp.ok ) throw new Error('Error uploading images')
+        const cloudResp = await resp.json();
+
+        return cloudResp.secure_url;
+
+    } catch (error) {
+        console.log(error);
+        throw new Error( error.message );
+    }
+
+}
