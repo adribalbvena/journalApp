a83a7696233b2fecf27d23d2af9bbe04cb7a81c5
Adriana Balbuena <adribalbuena@hotmail.com>
2022-12-30 21:31:36 +0100

    preparing files to deployment

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,27 +2,18 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite'
-import { getEnvironments } from '../helpers';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const {
-VITE_FIREBASE_APIKEY,
-VITE_FIREBASE_AUTHDOMAIN,
-VITE_FIREBASE_PROJECTID,
-VITE_FIREBASE_STORAGEBUCKET,
-VITE_FIREBASE_MESSAGINGSENDERID,
-VITE_FIREBASE_APPID,
-} = getEnvironments();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: VITE_FIREBASE_APIKEY,
-  authDomain: VITE_FIREBASE_AUTHDOMAIN,
-  projectId: VITE_FIREBASE_PROJECTID,
-  storageBucket: VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: VITE_FIREBASE_MESSAGINGSENDERID,
-  appId: VITE_FIREBASE_APPID
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID
 };
 
 
